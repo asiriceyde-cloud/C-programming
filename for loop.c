80038d0fa4accf431ca81f10a5d08cff40239f61
asiriceyde-cloud <asiriceyde@gmail.com>
2025-10-29 20:22:47 +0300

    Add files via upload

---
+++ for loop.c
@@ -0,0 +1,20 @@
+// Description: A program using for loop to countdown from 100-50.
+
+// Name: Gladys Opondo
+ //Reg No.:PA106/G/28789/25
+
+
+#include <stdio.h>
+
+int main(){
+	 
+	int i;
+	
+//for loop to countdown from 100-50
+for(i=100;i>=50;i--){
+	
+	printf("%d\n",i);
+}
+	
+	return 0;
+}
